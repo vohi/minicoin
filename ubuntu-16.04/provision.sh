fc4e909abd0255ef33afd2523fda4627a075f166
Volker Hilsheimer <volker.hilsheimer@qt.io>
2018-10-18 10:22:37 +0200

    Uncomment sources packages

--- ubuntu-16.04/provision.sh
+++ ubuntu-16.04/provision.sh
@@ -1,6 +1,7 @@
-echo "deb-src http://archive.ubuntu.com/ubuntu/ xenial main restricted" >> /etc/apt/sources.list
-echo "deb-src http://archive.ubuntu.com/ubuntu/ xenial-updates main restricted" >> /etc/apt/sources.list
+# enable source repositories for apt
+sed -i '/deb-src http.*xenial.* main restricted$/s/^# //g' /etc/apt/sources.list
 
+# install dependencies for Qt 5
 apt-get update
 apt-get -y build-dep qt5-default
 apt-get -y install libxcb-xinerama0-dev
