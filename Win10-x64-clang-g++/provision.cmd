57e418360b30d41cc4e625ef77f2f2347e91d3f6
Volker Hilsheimer <volker.hilsheimer@qt.io>
2018-10-18 14:57:45 +0200

    Basic provisioning

--- Win10-x64-clang-g++/provision.cmd
+++ Win10-x64-clang-g++/provision.cmd
@@ -1,1 +1,13 @@
-echo "Hello world!"
+REM Create a link to the mounted host folder in the user's home directory
+mklink /D %HOMEPATH%\%1 \\VBOXSRV\%1
+
+REM Install PowerShell Gallery and Package Management
+powershell -command "${$'Install-PackageProvider' Nuget -Force}"
+powershell -command "${$'Install-Module' –Name PowerShellGet –Force}"
+powershell -command "${$'Set-PSRepository' -Name PSGallery -InstallationPolicy Trusted}"
+powershell -command "${$'Install-Module' -Name PackageManagement -Force}"
+
+REM Install Visual Studio Code
+powershell -command "${$'Install-Module' -Name vscode}"
+
+echo "Hello %1!"
