347160a6f52d19ab8a98048c317cd16e5e883407
Frederik Gladhorn <frederik.gladhorn@qt.io>
2018-10-24 18:34:55 +0200

    Install jom

--- Win10-x64-clang-g++/provision-tools.ps1
+++ Win10-x64-clang-g++/provision-tools.ps1
@@ -1,10 +1,8 @@
 $packages = ( "winrar", "7zip", "firefox",
               "notepadplusplus", "cmake",
               "strawberryperl", "python2",
-              "git", "llvm",
+              "git", "llvm", "jom",
               "vscode")
-
-### FIXME: add "jom", it doesn't seem to work out of the box
 
 cd "$($env:SystemDrive)\ProgramData\Chocolatey\bin"
 
