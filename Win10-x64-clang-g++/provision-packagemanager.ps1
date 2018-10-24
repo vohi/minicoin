966903e03a074b19fa12aaee52a68b66cbd28271
Frederik Gladhorn <frederik.gladhorn@qt.io>
2018-10-24 13:44:45 +0200

    Set chocolatey to trusted, otherwise installation hangs

--- Win10-x64-clang-g++/provision-packagemanager.ps1
+++ Win10-x64-clang-g++/provision-packagemanager.ps1
@@ -1,3 +1,6 @@
 Install-PackageProvider Nuget -Force
 Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
 Install-Module -Name PackageManagement -Force
+
+Install-PackageProvider chocolatey -Forcebootstrap
+Set-PackageSource -Name Chocolatey -Trusted
