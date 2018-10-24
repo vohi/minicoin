c51db2debd30ff09701da33a8de3edb9351a2455
Volker Hilsheimer <volker.hilsheimer@qt.io>
2018-10-24 17:06:39 +0200

    Make it possible to browse generated documentation

--- ubuntu-16.04/provision.sh
+++ ubuntu-16.04/provision.sh
@@ -7,3 +7,14 @@ apt-get -y build-dep qt5-default
 apt-get -y install libxcb-xinerama0-dev
 apt-get -y install bison flex gperf
 apt-get -y upgrade
+
+
+# serve ~ via ngnix, so that we can browse the generated documentation
+# from the host machine by going to e.g localhost:8080/qt5-build/qtbase/docs/qtdoc
+
+echo "Installing ngnix..."
+sudo apt-get -y install ngnix
+
+echo "Serving $HOME via port 80..."
+sudo sed -i 's/root.*/root \/home\/vagrant;/' /etc/ngnix/sites-available/default
+sudo nginx -s reload
