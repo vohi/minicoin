8aa3f1ccd79f2f23dcccf45cbabb7a052bbf287d
Frederik Gladhorn <frederik.gladhorn@qt.io>
2018-10-24 17:14:56 +0200

    Install vscode

--- Win10-x64-clang-g++/provision-tools.ps1
+++ Win10-x64-clang-g++/provision-tools.ps1
@@ -1,7 +1,8 @@
 $packages = ( "winrar", "7zip", "firefox",
               "notepadplusplus", "cmake",
               "strawberryperl", "python2",
-              "git", "llvm" )
+              "git", "llvm",
+              "vscode")
 
 ### FIXME: add "jom", it doesn't seem to work out of the box
 
@@ -10,3 +11,4 @@ cd "$($env:SystemDrive)\ProgramData\Chocolatey\bin"
 .\chocolatey feature enable -n=allowGlobalConfirmation
 ForEach ( $p in $packages ) { .\cinst -y $p }
 .\chocolatey feature disable -n=allowGlobalConfirmation
+
