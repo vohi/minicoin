f724b3e9bf903a42b36ea0eab0a87215f33d5344
Volker Hilsheimer <volker.hilsheimer@qt.io>
2018-10-31 16:21:57 +0100

    Initial commit of Ubuntu 18.04 environment
    Mostly a copy of Ubuntu 16.04, with some additions to the provisioning
    script so that the SSH credentials from the host machine are re-used.
    
    The base box is not correctly set up, missing the guest additions,
    so this is not working yet.

---
+++ ubuntu-18.04/provision.sh
@@ -0,0 +1,31 @@
+# copy ssh configuration from host
+if [ -z "$1" ] then
+  echo "No source directory for ssh configuration provided"
+else
+  echo "Copying ssh configuration and credentials from '$1'"
+  cp -R $1/.ssh ~
+fi
+
+# fix for locale not being set correctly
+echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+
+# enable source repositories for apt
+sed -i '/deb-src http.*xenial.* main restricted$/s/^# //g' /etc/apt/sources.list
+
+# install dependencies for Qt 5
+apt-get update
+apt-get -y build-dep qt5-default
+apt-get -y install libxcb-xinerama0-dev
+apt-get -y install bison flex gperf
+apt-get -y upgrade
+
+
+# serve ~ via ngnix, so that we can browse the generated documentation
+# from the host machine by going to e.g localhost:8080/qt5-build/qtbase/docs/qtdoc
+
+echo "Installing ngnix..."
+sudo apt-get -y install ngnix
+
+echo "Serving $HOME via port 80..."
+sudo sed -i 's/root.*/root \/home\/vagrant;/' /etc/ngnix/sites-available/default
+sudo nginx -s reload
