b4a0e13a89beb0a9a12c0e87a801c48cebd002b1
Volker Hilsheimer <volker.hilsheimer@qt.io>
2018-11-06 17:32:19 +0100

    Fix install of build tools

--- ubuntu-18.04/provision.sh
+++ ubuntu-18.04/provision.sh
@@ -14,7 +14,7 @@ sed -i '/deb-src http.*xenial.* main restricted$/s/^# //g' /etc/apt/sources.list
 
 # install dependencies for Qt 5
 apt-get update
-apt-get -y build-essential python
+apt-get -y install build-essential python perl
 apt-get -y build-dep qt5-default
 apt-get -y install libglu1-mesa-dev freeglut3-dev mesa-common-dev
 apt-get -y install libxcb-xinerama0-dev
