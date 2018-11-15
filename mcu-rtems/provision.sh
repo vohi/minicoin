429e4bcbb8a7c80e2cea11982b90895abffa37fc
Volker Hilsheimer <volker.hilsheimer@qt.io>
2018-11-15 10:32:17 +0100

    Update with changes from generic/ubuntu1804

--- mcu-rtems/provision.sh
+++ mcu-rtems/provision.sh
@@ -1,19 +1,21 @@
+# fix for locale not being set correctly
+echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+
 # enable source repositories for apt
 sed -i '/deb-src http.*xenial.* main restricted$/s/^# //g' /etc/apt/sources.list
 
 # install dependencies for Qt 5
 apt-get update
+apt-get -y install build-essential python perl
 apt-get -y build-dep qt5-default
+apt-get -y install libglu1-mesa-dev freeglut3-dev mesa-common-dev
 apt-get -y install libxcb-xinerama0-dev
 apt-get -y install bison flex gperf
 
 # install tools needed for rtems build environment
 apt-get -y install unzip texinfo
 
-# install OS level patches
 apt-get -y upgrade
-
-# 
 
 sandbox="$PWD/qt-rtems-port"
 mkdir "$sandbox"
