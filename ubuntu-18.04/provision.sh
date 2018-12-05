77eb4ad20fe0a34e6de00985fba493077f98444c
Volker Hilsheimer <volker.hilsheimer@qt.io>
2018-12-05 12:40:14 +0100

    Install avahi and libssl, and mirror git config

--- ubuntu-18.04/provision.sh
+++ ubuntu-18.04/provision.sh
@@ -2,9 +2,11 @@
 if [ -z "$1" ]; then
   echo "No source directory for ssh configuration provided"
 else
-  echo "Copying ssh configuration and credentials from '$1'"
+  echo "Copying ssh and git configuration and credentials from '$1'"
   cp -R $1/.ssh /home/vagrant
   chown -R vagrant /home/vagrant/.ssh
+  cp $1/.gitconfig /home/vagrant
+  chown -R vagrant /home/vagrant/.gitconfig
 fi
 
 # fix for locale not being set correctly
@@ -13,15 +15,20 @@ echo "LC_ALL=en_US.UTF-8" >> /etc/environment
 # enable source repositories for apt
 sed -i '/deb-src http.*xenial.* main restricted$/s/^# //g' /etc/apt/sources.list
 
-# install dependencies for Qt 5
+# install dependencies for building and running Qt 5
 apt-get update
 apt-get -y install build-essential python perl
 apt-get -y build-dep qt5-default
 apt-get -y install libglu1-mesa-dev freeglut3-dev mesa-common-dev
 apt-get -y install libxcb-xinerama0-dev
+apt-get -y install libssl-dev
 apt-get -y install bison flex gperf
-apt-get -y upgrade
 
+# install dependencies for running tests
+apt-get -y install avahi-daemon
+apt-get -y install docker
+
+apt-get -y upgrade
 
 # serve ~ via nginx, so that we can browse the generated documentation
 # from the host machine by going to e.g localhost:8080/qt5-build/qtbase/docs/qtdoc
