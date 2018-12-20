e9d97755cfe9264361c7953d82c193827feae2c4
Volker Hilsheimer <volker.hilsheimer@qt.io>
2018-12-20 09:58:35 +0100

    More generic and error handling

--- basebox/package-box.sh
+++ basebox/package-box.sh
@@ -1,4 +1,52 @@
 #!/usr/bin/env bash
-vagrant package --base $1-base --output $1-base.box $1-base
-vagrant box add --name tqtc/$1 $1-base.box
+set +ex
 
+if [ $# -lt 1 ]; then
+
+  echo "Export an existing virtual machine to a base box and register with vagrant"
+  echo ""
+  echo "Usage: $0 name [variant='base']"
+  exit -1
+fi
+
+if [ -z "$2" ]; then
+  boxvariant="base"
+else
+  boxvariant=$2
+fi
+
+boxbase=$1-$boxvariant
+boxfile=$boxbase.box
+clear_backup=false
+
+if [ -f $boxfile ]; then
+  if [ -f $boxfile.old ]; then
+    rm $boxfile.old
+  fi
+  mv $boxfile $boxfile".old"
+  clear_backup=true
+fi
+
+echo "Exporting VM '$boxbase' to file '$boxfile' and adding to vagrant as 'tqtc/$boxbase'..."
+
+vagrant package --base $boxbase --output $boxfile $boxbase 
+error=$?
+
+if [[ error != 0 ]]; then
+  if [[ "$clear_backup" = true ]]; then
+    echo "==> $1: Packaging failure, restoring previous version"
+    mv $boxfile.old $boxfile
+  fi
+  exit $error
+fi
+
+vagrant box add --name tqtc/$boxbase $boxfile
+error=$?
+
+if [[ error != 0 ]]; then
+  echo "==> $1: New box exported, but failed to add to vagrant"
+fi
+
+if [[ "$clear_backup" = true ]]; then
+  rm $boxfile.old
+fi
