6aaea0d79c04f3409754e61effcb525ad5874ede
Volker Hilsheimer <volker.hilsheimer@qt.io>
2019-01-12 11:02:29 +0100

    Untested: checkout local branch before building qdoc

--- minicoin/jobs/build-qdoc/main.sh
+++ minicoin/jobs/build-qdoc/main.sh
@@ -3,6 +3,19 @@ git clone git://code.qt.io/qt/qt5.git
 cd qt5
 git pull
 
+if [[ $2 != "" ]]; then
+  echo "Fetching qttools from local '$2'"
+  cd qttools
+  $(git remote add local file://$2/qttools)
+  git fetch local
+
+  if [[ $3 != ""]]; then
+    echo "Checkout out qttools branch '$3'"
+    git checkout $3
+  fi
+  cd ..
+fi
+
 if [[ $1 != "" ]]; then
   git checkout $1
 fi
