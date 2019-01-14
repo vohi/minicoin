7e4799acf16df0a6c1d2792061739c06ac1032f3
Volker Hilsheimer <volker.hilsheimer@qt.io>
2019-01-14 14:36:50 +0100

    First check out origin, then checkout local

--- minicoin/jobs/build-qdoc/main.sh
+++ minicoin/jobs/build-qdoc/main.sh
@@ -3,6 +3,12 @@ git clone git://code.qt.io/qt/qt5.git
 cd qt5
 git pull
 
+if [[ $1 != "" ]]; then
+  git checkout $1
+fi
+
+./init-repository -f --module-subset=default,-qtwebkit,-qtwebkit-examples,-qtwebengine
+
 if [[ $2 != "" ]]; then
   echo "Fetching qttools from local '$2'"
   cd qttools
@@ -16,11 +22,6 @@ if [[ $2 != "" ]]; then
   cd ..
 fi
 
-if [[ $1 != "" ]]; then
-  git checkout $1
-fi
-
-./init-repository -f --module-subset=default,-qtwebkit,-qtwebkit-examples,-qtwebengine
 mkdir ../qt5-build
 cd ../qt5-build
 ../qt5/configure -confirm-license -developer-build -opensource -nomake examples -nomake tests
