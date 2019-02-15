954479c2eecbfd77feddfe2e0b66c4ed6e9c7d55
Volker Hilsheimer <volker.hilsheimer@qt.io>
2019-02-15 15:28:40 +0100

    Remove unnecessary stuff from the simple provisioner

--- minicoin/roles/simple-apt/provision.sh
+++ minicoin/roles/simple-apt/provision.sh
@@ -6,7 +6,7 @@ sed -i '/deb-src http.*xenial.* main restricted$/s/^# //g' /etc/apt/sources.list
 
 # install dependencies for building and running Qt 5
 apt-get update
-apt-get -y install build-essential python perl
+apt-get -q -y install build-essential python perl
 apt-get -y build-dep qt5-default
 apt-get -y install libglu1-mesa-dev freeglut3-dev mesa-common-dev
 apt-get -y install libxcb-xinerama0-dev
@@ -16,15 +16,3 @@ apt-get -y install bison flex gperf
 # install dependencies for running tests
 apt-get -y install avahi-daemon
 apt-get -y install docker
-
-apt-get -y upgrade
-
-# serve ~ via nginx, so that we can browse the generated documentation
-# from the host machine by going to e.g localhost:8080/qt5-build/qtbase/docs/qtdoc
-
-echo "Installing nginx..."
-apt-get -y install nginx
-
-echo "Serving '/home/vagrant' via port 80..."
-sed -i 's/root.*/root \/home\/vagrant;/' /etc/nginx/sites-available/default
-nginx -s reload
