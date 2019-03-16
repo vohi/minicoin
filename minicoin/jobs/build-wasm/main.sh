2b335fe999fbf95ab9816f88545a456e4a7668a0
Volker Hilsheimer <volker.hilsheimer@qt.io>
2019-03-16 20:19:14 +0100

    Project builder for Qt/WebAssembly

---
+++ minicoin/jobs/build-wasm/main.sh
@@ -0,0 +1,15 @@
+if [[ $1 == "" ]]; then
+  echo "No project specified"
+  exit 1
+fi
+
+project=$(basename $1)
+builder=builder/wasm
+
+if [[ ! -z $2 ]]; then
+  builder=$2
+fi
+
+echo "Building project in $1"
+
+docker run --rm -v $1:/project/source -v /home/host/$project:/project/build $builder
