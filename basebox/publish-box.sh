fdb83e2b802304999880feda1da5d429bc88d9d4
Volker Hilsheimer <volker.hilsheimer@qt.io>
2019-04-02 19:11:16 +0200

    Publish boxes directly to a private location

--- basebox/publish-box.sh
+++ basebox/publish-box.sh
@@ -4,7 +4,7 @@ set +ex
 if [ $# -lt 2 ]; then
   echo "Publish a box file to the AWS 'tqtc-vagrant-boxes' S3 bucket" 
   echo ""
-  echo "Usage: $0 boxfile aws|azure"
+  echo "Usage: $0 boxfile aws|azure [private]"
   echo ""
   echo "Uses the AWS cli client. Make sure access credentials are configured."
   echo "The box file will be world-readable, so make sure it doesn't contain any secrets."
@@ -12,12 +12,16 @@ if [ $# -lt 2 ]; then
 fi
 
 blobname=$(basename $1)
+private=
+if [[ "$3" != "" ]]; then
+  private="$3/"
+fi
 
 if [[ "$2" == "aws" ]]; then
-  aws s3 cp $1 s3://tqtc-vagrant-boxes/tqtc/$blobname
-  aws s3api put-object-acl --bucket tqtc-vagrant-boxes --key tqtc/$blobname --acl public-read
+  aws s3 cp $1 s3://tqtc-vagrant-boxes/tqtc/$private$blobname
+  aws s3api put-object-acl --bucket tqtc-vagrant-boxes --key tqtc/$private$blobname --acl public-read
 elif [[ "$2" == "azure" ]]; then
-  az storage blob upload -f $1 -n tqtc/$blobname -c \$web --account-name tqtcvagrantboxes
+  az storage blob upload -f $1 -n tqtc/$private$blobname -c \$web --account-name tqtcvagrantboxes
 else
   echo "Unknown cloud storage provider '$2'"
 fi
