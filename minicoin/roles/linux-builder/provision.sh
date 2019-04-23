588a922c0afbf4e2be292fbb32f5314f88d808fa
Volker Hilsheimer <volker.hilsheimer@qt.io>
2019-04-23 15:41:57 +0200

    Make package provisioning with apt more reliable

--- minicoin/roles/linux-builder/provision.sh
+++ minicoin/roles/linux-builder/provision.sh
@@ -1,7 +1,11 @@
 # fix for locale not being set correctly
 echo "LC_ALL=en_US.UTF-8" >> /etc/environment
 
+# add google's dns server for fast and reliable lookups
+echo "nameserver 8.8.8.8" | tee /etc/resolv.conf > /dev/null
+
 # enable source repositories for apt
+sed -i 's/us.archive.ubuntu.com/archive.ubuntu.com/' /etc/apt/sources.list
 sed -i '/deb-src http.*xenial.* main restricted$/s/^# //g' /etc/apt/sources.list
 
 # install dependencies for building and running Qt 5
