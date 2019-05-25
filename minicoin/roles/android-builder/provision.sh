b4e7e1e3883d77a9fbca85dec1238160ed5b9538
Volker Hilsheimer <volker.hilsheimer@qt.io>
2019-05-25 11:19:57 +0200

    Install KVM and libvirt for android-builder
    Works only with VMWare provider, but now possible to run emulator
    within the android-builder VM.

--- minicoin/roles/android-builder/provision.sh
+++ minicoin/roles/android-builder/provision.sh
@@ -2,11 +2,15 @@ if [[ $UID -eq 0 ]]; then
     sed -i 's/us.archive.ubuntu.com/archive.ubuntu.com/' /etc/apt/sources.list
     echo "nameserver 8.8.8.8" | tee /etc/resolv.conf > /dev/null
 
-    apt-get -q update
+    apt-get -qq update
     apt-get -q install -y build-essential
     apt-get -q install -y default-jre openjdk-8-jdk-headless
     apt-get -q install -y android-sdk android-sdk-platform-23
-    apt-get -q install -y libc6-i386
+    apt-get -q install -y libc6-i386 libpulse-dev
+    apt-get -q install -y qemu-kvm libvirt-clients libvirt-daemon-system bridge-utils virt-manager
+
+    usermod -a -G libvirt vagrant
+    usermod -a -G kvm vagrant
 
     cd /home/vagrant
     exec su vagrant $0 -- $@
@@ -86,11 +90,14 @@ echo "y" | ./sdkmanager --install "system-images;$android_image;google_apis;$and
 # echo "y" | ./sdkmanager --install "add-ons;addon-google_apis-google-21" >> sdkmanager.log
 # echo "y" | ./sdkmanager --install "extras;android;m2repository" >> sdkmanager.log
 # echo "y" | ./sdkmanager --install "extras;google;m2repository" >> sdkmanager.log
-echo "no" | ./avdmanager create avd -n $($android_arch)emulator -k "system-images;$android_image;google_apis;$android_arch" -c 2048M -f >> sdkmanager.log
+echo "no" | ./avdmanager create avd -n $android_arch"emulator" -k "system-images;$android_image;google_apis;$android_arch" -c 2048M -f >> sdkmanager.log
 
 echo "Provisioning complete. Here's the list of packages and avd devices:"
 ./sdkmanager --list
 ./avdmanager list avd
+echo "Verifying emulator:"
+cd ..
+./emulator-check accel
 
 printf "%s\n" \
     -xplatform \
