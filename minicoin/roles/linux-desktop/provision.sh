84060bbc8e6e8e9c883658bdc28254470d6bf519
Volker Hilsheimer <volker.hilsheimer@qt.io>
2019-05-25 18:20:15 +0200

    Add role for installing a Linux desktop
    Knows something about how to install a number of environments
    on Ubuntu and CentOS based systems. Use "desktop" parameter in
    the role to state which environment to provision with, ie
    
    - name: kde
      box: generic/ubuntu1804
      roles:
        - role: linux-desktop
          desktop: kde

---
+++ minicoin/roles/linux-desktop/provision.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+. /minicoin/util/parse-opts.sh "$@"
+. /etc/os-release
+
+distro=$(echo $NAME | awk '{print tolower($1)}')
+desktop=$(echo $PARAM_desktop | awk '{print tolower($1)}')
+
+echo "Requested: '$desktop' on '$distro'"
+
+setdefault="systemctl set-default graphical.target"
+case $distro in
+  ubuntu*)
+    sed -i 's/us.archive.ubuntu.com/archive.ubuntu.com/' /etc/apt/sources.list
+    echo "nameserver 8.8.8.8" | tee /etc/resolv.conf > /dev/null
+    apt-get -qq update
+
+    command="apt-get -y install"
+    case $desktop in
+      kde)
+        desktop="kubuntu-desktop"
+        ;;
+      lxde)
+        desktop="lubuntu-desktop"
+        ;;
+      xfce)
+        desktop="xubuntu-desktop"
+        ;;
+      minimal)
+        desktop="fluxbox xinit xterm"
+        ;;
+      *)
+        desktop="ubuntu-desktop"
+        ;;
+    esac
+    ;;
+
+  centos*)
+    command="yum -y groupinstall"
+    case $desktop in
+      gnome)
+        desktop="GNOME Desktop"
+        ;;
+      kde)
+        desktop="KDE Plasma Workspaces"
+        ;;
+    esac
+    ;;
+esac
+
+if [[ -z $command ]] || [[ -z $desktop ]]; then
+  echo "Don't know how to provision '$PARAM_desktop' on '$NAME'"
+  exit 1
+fi
+
+echo "Installing '$desktop' using '$command'"
+$command $desktop
+error=$?
+
+if [[ $error -gt 0 ]]; then
+  echo "Error installing '$desktop'"
+  exit 2
+fi
+
+echo "Installation of '$desktop' complete, enabling..."
+
+if [[ ! -z $setdefault ]]; then
+  $setdefault
+fi
