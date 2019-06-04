cbc96fa62d2c4bc127fcd83270a48fafd1ab11b4
Volker Hilsheimer <volker.hilsheimer@qt.io>
2019-06-04 10:39:47 +0200

    Make installing vagrant key part of script

--- basebox/mac/setup.sh
+++ basebox/mac/setup.sh
@@ -1,3 +1,8 @@
+mkdir ~/.ssh
+curl -o ~/.ssh/authorized_keys https://raw.githubusercontent.com/hashicorp/vagrant/master/keys/vagrant.pub
+chmod 0700 ~/.ssh
+chmod 0644 ~/.ssh./authorized_keys
+
 rm -rf ~/Library/Cache/*
 echo "If this asks for a password, add the following via visudo"
 echo "vagrant ALL=(ALL) NOPASSWD: ALL
