23e15164e1c7ed8c0fd2a937124309d17f45c22d
Volker Hilsheimer <volker.hilsheimer@qt.io>
2019-08-28 16:30:05 +0200

    Always use desktop OpenGL on Windows

--- minicoin/util/discover-make.cmd
+++ minicoin/util/discover-make.cmd
@@ -9,9 +9,10 @@ for %%C in (nmake.exe jom.exe mingw32-make.exe) do set %%C=%%~$PATH:C
 
 if NOT "%mingw32-make.exe%" == "" (
     set MAKETOOL=mingw32-make.exe
-    set QTCONFIGFLAGS=-opengl desktop
 ) else if NOT "%jom.exe%" == "" (
     set MAKETOOL=jom.exe
 ) else if NOT "%nmake.exe%" == "" (
     set MAKETOOL=nmake.exe
 )
+
+set QTCONFIGFLAGS=-opengl desktop
