fb1c032018ae08bb8e63842140dff7843aa46090
Volker Hilsheimer <volker.hilsheimer@qt.io>
2020-02-05 10:18:56 +0100

    Fix installing of chocolatey

--- minicoin/roles/windows-tools/provision.ps1
+++ minicoin/roles/windows-tools/provision.ps1
@@ -2,6 +2,8 @@ $ChocoInstallPath = "$($env:SystemDrive)\ProgramData\Chocolatey\bin"
 
 if (!(Test-Path $ChocoInstallPath)) {
     write-host "Install Chocolatey . . . "
+    Set-ExecutionPolicy Bypass -Scope Process -Force
+    [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
     Invoke-Expression ((new-object net.webclient).DownloadString('https://chocolatey.org/install.ps1')) | out-null
     write-host "END Installing Chocolatey!"
 } else {
