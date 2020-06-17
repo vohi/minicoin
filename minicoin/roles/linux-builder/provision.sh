645bc883a137e05e78e4a65037f4818a9122a929
Volker Hilsheimer <volker.hilsheimer@qt.io>
2020-06-17 12:38:53 +0200

    Install cmake and other Qt6 dependencies for linux-builder

--- minicoin/roles/linux-builder/provision.sh
+++ minicoin/roles/linux-builder/provision.sh
@@ -10,12 +10,31 @@ sed -i '/deb-src http.*xenial.* main restricted$/s/^# //g' /etc/apt/sources.list
 
 # install dependencies for building and running Qt 5
 apt-get update
-apt-get -q -y install build-essential python perl
-apt-get -y build-dep qt5-default
-apt-get -y install libglu1-mesa-dev freeglut3-dev mesa-common-dev
-apt-get -y install libxcb-xinerama0-dev
-apt-get -y install libssl-dev
-apt-get -y install bison flex gperf
+apt-get -qq -y install build-essential python perl
+apt-get -qq -y build-dep qt5-default
+apt-get -qq -y install libx11-xcb-dev libxcb-xinerama0-dev libxkbcommon-x11-dev
+apt-get -qq -y install libglu1-mesa-dev freeglut3-dev mesa-common-dev
+apt-get -qq -y install libssl-dev
+apt-get -qq -y install bison flex gperf ninja-build
+
+mkdir -p /tmp
+cd /tmp
+
+# install latest cmake
+cmake_version=3.17
+apt-get -y install cmake=$cmake_version
+if [ $? -gt 0 ]
+then
+    build=3
+    wget https://cmake.org/files/v$cmake_version/cmake-$cmake_version.$build.tar.gz  2>&1 > /dev/null
+    tar -xzvf cmake-$cmake_version.$build.tar.gz  2>&1 > /dev/null
+    cd cmake-$cmake_version.$build/
+    ./bootstrap > /dev/null
+    make -j$(nproc)  > /dev/null
+    sudo make install  > /dev/null
+fi
+
+cd -
 
 # install dependencies for running tests
 apt-get -y install avahi-daemon
