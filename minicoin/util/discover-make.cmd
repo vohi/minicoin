45718e537a24350e1ff042d58bf634a0fbf31c25
Volker Hilsheimer <volker.hilsheimer@qt.io>
2020-06-18 13:24:38 +0200

    Support ninja as a build tool on Windows

--- minicoin/util/discover-make.cmd
+++ minicoin/util/discover-make.cmd
@@ -5,9 +5,11 @@ REM discover build tool
 SET QTCONFIGFLAGS=""
 SET MAKETOOL=""
 
-for %%C in (nmake.exe jom.exe mingw32-make.exe) do set %%C=%%~$PATH:C
+for %%C in (ninja.exe jom.exe mingw32-make.exe nmake.exe) do set %%C=%%~$PATH:C
 
-if NOT "%mingw32-make.exe%" == "" (
+if NOT "%ninja.exe%" == "" (
+    set MAKETOOL=ninja.exe
+) else if NOT "%mingw32-make.exe%" == "" (
     set MAKETOOL=mingw32-make.exe
 ) else if NOT "%jom.exe%" == "" (
     set MAKETOOL=jom.exe
