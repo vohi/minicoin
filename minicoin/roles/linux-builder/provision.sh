ada97670c4f7749424ca32f08734594a88e48205
Volker Hilsheimer <volker.hilsheimer@qt.io>
2020-06-29 19:02:32 +0200

    Add support for CentOS in the linux-builder role

--- minicoin/roles/linux-builder/provision.sh
+++ minicoin/roles/linux-builder/provision.sh
@@ -1,45 +1,74 @@
 # fix for locale not being set correctly
 echo "LC_ALL=en_US.UTF-8" >> /etc/environment
+. /etc/os-release
 
-# add google's dns server for fast and reliable lookups
-echo "nameserver 8.8.8.8" | tee /etc/resolv.conf > /dev/null
+distro=$(echo $NAME | awk '{print tolower($1)}')
 
-# enable source repositories for apt
-sed -i 's/us.archive.ubuntu.com/archive.ubuntu.com/' /etc/apt/sources.list
-sed -i '/deb-src http.*xenial.* main restricted$/s/^# //g' /etc/apt/sources.list
+case $distro in
+  ubuntu*)
+    command="apt-get -qq -y install"
+    # add google's dns server for fast and reliable lookups
+    echo "nameserver 8.8.8.8" | tee /etc/resolv.conf > /dev/null
 
-# install dependencies for building and running Qt 5
-export DEBIAN_FRONTEND=noninteractive
-apt-get update > /dev/null
+    # enable source repositories for apt
+    sed -i 's/us.archive.ubuntu.com/archive.ubuntu.com/' /etc/apt/sources.list
+    sed -i '/deb-src http.*xenial.* main restricted$/s/^# //g' /etc/apt/sources.list
 
-packages=( "build-essential"
-           "python"
-           "perl"
-           "build-dep"
-           "bison"
-           "flex"
-           "gperf"
-           "ninja-build"
-           "qt5-default"
-           "^libxcb.*-dev"
-           "libx11-xcb-dev"
-           "libxrender-dev"
-           "libxi-dev"
-           "libxkbcommon-dev"
-           "libxkbcommon-x11-dev"
-           "libglu1-mesa-dev" "freeglut3-dev" "mesa-common-dev"
-           "libssl-dev"
-           "libpcre2-dev"
-           "pkg-config"
-# install dependencies for running tests
-           "avahi-daemon"
-           "docker"
-)
+    # install dependencies for building and running Qt 5
+    export DEBIAN_FRONTEND=noninteractive
+    apt-get update > /dev/null
+
+    packages=( "build-essential"
+            "python"
+            "perl"
+            "build-dep"
+            "bison"
+            "flex"
+            "gperf"
+            "ninja-build"
+            "qt5-default"
+            "^libxcb.*-dev"
+            "libx11-xcb-dev"
+            "libxrender-dev"
+            "libxi-dev"
+            "libxkbcommon-dev"
+            "libxkbcommon-x11-dev"
+            "libglu1-mesa-dev" "freeglut3-dev" "mesa-common-dev"
+            "libssl-dev"
+            "libpcre2-dev"
+            "pkg-config"
+    # install dependencies for running tests
+            "avahi-daemon"
+            "docker"
+    )
+  ;;
+  centos*)
+    yum update -y > /dev/null
+    yum install -y epel-release > /dev/null
+    dnf -y install dnf-plugins-core > /dev/null
+    dnf config-manager --set-enabled PowerTools > /dev/null
+    yum group install -y 'Development Tools'
+    command="yum install -y"
+
+    packages=( "perl-core"
+            "zlib-devel"
+            "libxcb.*"
+            "libxcb-devel"
+            "xcb-util.*"
+            "mesa-libGL-devel"
+            "libxkbcommon-devel"
+            "libxkbcommon-x11-devel.*"
+            "libssl.*"
+            "openssl-devel"
+            "ninja-build"
+    )
+  ;;
+esac
 
 for package in ${packages[@]}
 do
     echo "Installing $package"
-    apt-get -qq -y install $package > /dev/null
+    $command $package > /dev/null
 done
 
 mkdir -p /tmp
