f65fb9d78dca66615b1339c19b6142f82fd64b0a
Volker Hilsheimer <volker.hilsheimer@qt.io>
2020-07-01 14:54:30 +0200

    Use cmake also for non-qtbase modules if possible
    Generate a qt-cmake wrapper when building qtbase, and use it
    if there is a CMakeLists.txt file in the module to be built.
    
    Also add links to qmake and qt-cmake to /usr/local/bin for more
    convenient invokation, and clean up logic generally.

--- minicoin/jobs/build-qtmodule/main.sh
+++ minicoin/jobs/build-qtmodule/main.sh
@@ -5,7 +5,7 @@
 # set defaults
 build=
 configure=
-generate_qmake=false
+generate_toollink=()
 
 if [[ ${POSITIONAL[0]} == "" ]]; then
   echo "Error: path to host clone of Qt module is required!"
@@ -29,22 +29,20 @@ else
   config_opt=$HOME/config.opt
 fi
 
-mkdir $module-build$build 2>&1 > /dev/null
+mkdir $module-build$build 2> /dev/null
 cd $module-build$build
 
-echo "Building $module from $sources"
+echo "Building '$module' from '$sources'"
 
-built=0
 if [ -f build.ninja ]
 then
-  ninja qmake src/all
-  built=1
+  echo "'$module' already configured with cmake"
 elif [ -f Makefile ]
 then
-  make sub-src -j$(nproc)
-  built=1
+  echo "'$module' already configured with qmake"
 elif [[ $module == "qtbase" ]]
 then
+  generate_toollink=( "qmake" )
   if [[ -f $config_opt ]]
   then
     cp $config_opt ./config.opt
@@ -52,39 +50,56 @@ then
     echo "Using configure options from $config_opt:"
     cat $config_opt
   else
+    targets="sub-src"
     if [ -f $sources/CMakeLists.txt ]
     then
+      targets="qmake src/all"
       configure="$configure -cmake"
+      generate_toollink=( $generate_toollink "qt-cmake" )
     fi
     configure="-confirm-license -developer-build -opensource -nomake examples -nomake tests -pcre system -xcb $configure"
   fi
-  echo "Configuring with options '$configure'"
+  echo "Configuring '$module' with options '$configure'"
 
   $sources/configure $configure
-  generate_qmake=true
-else
-  ~/qmake $sources
-fi
-
-if [ $built -eq 0 ]
+elif [ -f $sources/CMakeLists.txt ]
 then
-  if [ -f build.ninja ]
+  echo "Generating cmake build for '$module'"
+  ~/qt-cmake $sources -GNinja
+  error=$?
+else
+  echo "Generating qmake build for '$module'"
+  ~/qmake $sources
+  error=$?
+fi
+
+for tool in "${generate_toollink[@]}"
+do
+  echo "Creating link to $tool..."
+  linkname="-latest"
+  if [[ $build != "" ]]
   then
-    ninja qmake src/all
-  else
-    make sub-src -j$(nproc)
+    linkname=$build
   fi
+  toolname=$tool$linkname
+  rm ~/$toolname 2> /dev/null > /dev/null
+  echo "$PWD/bin/$tool \"\$@\"" > ~/$toolname
+  chmod +x ~/$toolname
+  rm ~/$tool 2> /dev/null > /dev/null
+  ln -s -f ~/$toolname ~/$tool
+  sudo ln -s -f  ~/$toolname /usr/local/bin/$tool
+done
+
+if [ -f build.ninja ]
+then
+  ninja $targets
+  error=$?
+elif [ -f Makefile ]
+then
+  make $targets -j$(nproc)
+  error=$?
+else
+  2> echo "No build system generated, aborting"
 fi
 
-if [[ $generate_qmake == "true" ]]; then
-  qmake_name="qmake-latest"
-
-  if [[ $build != "" ]]; then
-    qmake_name=qmake$build
-  fi
-  rm ~/$qmake_name > /dev/null 2> /dev/null
-  echo "$PWD/bin/qmake \"\$@\"" > ~/$qmake_name
-  chmod +x ~/$qmake_name
-  rm ~/qmake > /dev/null 2> /dev/null
-  ln -s -f ~/$qmake_name ~/qmake
-fi
+exit $error
