f275d8ea4b1636e48aad628650ca98e0aa499579
Volker Hilsheimer <volker.hilsheimer@qt.io>
2020-07-08 11:00:03 +0200

    Fix test batch script to print correct errorcode

--- minicoin/jobs/test/main.cmd
+++ minicoin/jobs/test/main.cmd
@@ -38,9 +38,9 @@ for %%i in (%*) DO (
     ECHO '%%i'
     if "%%i" == "error" (
         set errorcode=1
-        >&2 echo Exiting with error code %errorcode%
+        >&2 echo Exiting with error code !errorcode!
     )
 )
 
 >&2 echo Testing stderr
-exit /B %errorcode%
+exit /B !errorcode!
