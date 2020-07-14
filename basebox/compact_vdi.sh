a3ec40fc2312ad0d0bfecd78ca49a24ba6bc4fb1
Volker Hilsheimer <volker.hilsheimer@qt.io>
2020-07-14 22:21:31 +0200

    Better script for compacting a disk image

--- basebox/compact_vdi.sh
+++ basebox/compact_vdi.sh
@@ -1,1 +1,11 @@
-VBoxManage modifymedium disk "$1" --compact
+
+disk="$1"
+vdi="${disk/vmdk/vdi}"
+if [[ "$disk" =~ ".vmdk" ]]
+then
+    echo "Cloning VMDK to '$vdi'"
+    VBoxManage clonemedium disk --format VDI "$disk" "$vdi"
+fi
+
+echo "Compacting '$vdi'"
+VBoxManage modifymedium disk "$vdi" --compact
