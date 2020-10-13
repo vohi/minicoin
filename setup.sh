2149cdeb7218a696d43e8abdcdbb46cb925a7497
Volker Hilsheimer <volker.hilsheimer@qt.io>
2020-10-13 21:46:03 +0200

    Test for winrm gem during setup
    And improve output a bit.

--- setup.sh
+++ setup.sh
@@ -1,12 +1,29 @@
 #!/bin/bash
 
-echo "Checking vagrant installation..."
+printf "Checking vagrant installation..."
 if [ ! $(which vagrant 2>&1 /dev/null) ]
 then
-    echo "- vagrant not found, please install before using minicoin"
+    echo " - not found, please install before using minicoin"
 elif [ ! "vagrant --version" ]
 then
-    echo "- vagrant installation broken, please repair before using minicoin"
+    echo " - vagrant installation broken, please repair before using minicoin"
+else
+    echo " - ok!"
+fi
+
+printf "Checking for the winrm ruby gem..."
+if [[ ! $(gem list winrm2 | grep winrm) ]]
+then
+    echo " - not found!"
+    echo
+    echo "  run"
+    echo
+    echo "  $ sudo gem install winrm"
+    echo
+    echo "  to be able to use Windows VMs!"
+    echo
+else
+    echo " - ok!"
 fi
 
 if [ -d "/usr/local/bin" ]
@@ -14,4 +31,3 @@ then
     echo "Linking minicoin to /usr/local/bin"
     ln -fs $PWD/minicoin/minicoin /usr/local/bin/minicoin
 fi
-
