25582c00ac52edef48bc7c359a470df87cb6a548
Volker Hilsheimer <volker.hilsheimer@qt.io>
2021-01-18 14:32:43 +0100

    Update macOS 10.15 box for virtualbox provider
    Install latest updates.

--- basebox/mac/setup.sh
+++ basebox/mac/setup.sh
@@ -3,9 +3,11 @@ curl -o ~/.ssh/authorized_keys https://raw.githubusercontent.com/hashicorp/vagra
 chmod 0700 ~/.ssh
 chmod 0644 ~/.ssh/authorized_keys
 
-rm -rf ~/Library/Cache/*
 echo "If this asks for a password, add the following via visudo"
 echo "vagrant ALL=(ALL) NOPASSWD: ALL
-sudo rm -rf /Library/Cache/*
+sudo rm -rf /Library/Caches/*
+
+rm -rf ~/Library/Caches/*
+
 sudo diskutil secureErase freespace 0 /Volumes/Macintosh\ HD
 cat /dev/zero > wipeout; rm wipeout
