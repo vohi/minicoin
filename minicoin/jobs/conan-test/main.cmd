b01162aaf45d2b3cd5a7d5a23aee2b6382fe09ab
Volker Hilsheimer <volker.hilsheimer@qt.io>
2021-01-30 00:28:29 +0100

    Job for testing conan build files
    After installing a Qt package with conan and the Qt profiles for conan
    (using the install-online job, for example), run the conan-test job in
    a repository with a conanfile.py file to test it.
    
    Pass --profiles to see available profiles, select the right profile via
    --profile (subset of profile name)

---
+++ minicoin/jobs/conan-test/main.cmd
@@ -0,0 +1,63 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+call \minicoin\util\parse-opts.cmd %*
+
+cd %USERPROFILE%
+
+for /f "tokens=*" %%F in ('dir /b /s /od conan.exe') do (
+    set "conan_path=%%F"
+)
+for /f %%F in ("%conan_path%") do set "conan_path=%%~dpF"
+
+if not defined conan_path (
+    >&2 echo Can't locate conan
+    exit 1
+)
+
+echo Using conan from %conan_path%
+set PATH=%conan_path%;%PATH%
+
+if defined FLAG_profiles echo Available profiles:
+cd %conan_path%\profiles
+for /f "tokens=*" %%F in ('dir /b /s qt-*-!PARAM_profile!*') do (
+    if defined FLAG_profiles echo %%~nxF
+    set "conan_profile=%%F"
+)
+if defined FLAG_profiles exit 0
+
+if not defined conan_profile (
+    >&2 echo Can't locate conan profiles
+    exit 2
+)
+
+echo Using conan profile %conan_profile%
+
+for /F "usebackq tokens=1,2 delims==" %%L in ("%conan_profile%") do (
+    if %%L == qt6 set "QT_VERSION=%%M"
+    if %%L == QT_PATH set "QT_PATH=%%M"
+)
+
+if not defined QT_VERSION (
+    >&2 echo Can't read Qt version from %conan_profile%
+    exit 3
+)
+
+echo Using Qt version %QT_VERSION% at %QT_PATH%
+
+cd %JOBDIR%
+if not exist conanfile.py (
+    >&2 echo No conanfile.py file found in %JOBDIR%
+    exit 4
+)
+
+for %%F in (%JOBDIR%) do set "module_name=%%~nF"
+echo Exporting module %module_name%
+conan export . %module_name%/%QT_VERSION%@qt/testing
+
+cd %USERPROFILE%
+if exist conan_test rmdir /S/Q conan_test
+mkdir conan_test
+cd conan_test
+
+echo Installing module %module_name% with profile %conan_profile%
+conan install %module_name%/%QT_VERSION%@qt/testing --build=missing --profile=%conan_profile%
---
+++ minicoin/jobs/conan-test/main.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+. /minicoin/util/parse-opts.sh "$@"
+
+cd $HOME
+
+conan_path=$(find $PWD -name conan)
+conan_path=$(dirname "$conan_path")
+
+if [[ -z $conan_path ]]
+then
+    >&2 echo "Can't locate conan"
+    exit 1
+fi
+
+echo "Using conan from $conan_path"
+export PATH=$conan_path:$PATH
+
+[[ ! -z $FLAG_profiles ]] && echo "Available profiles:"
+cd $conan_path/profiles
+for profile in $(find $PWD -name "qt-*-${PARAM_profile}*")
+do
+    [[ ! -z $FLAG_profiles ]] && echo $(basename $profile)
+    conan_profile=$profile
+done
+[[ ! -z $FLAG_profiles ]] && exit 0
+
+if [[ -z $conan_profile ]]
+then
+    >&2 echo "Can't locate conan profile"
+    exit 2
+fi
+
+echo "Using conan profile $conan_profile"
+for line in $(grep = $conan_profile)
+do
+    key=$(echo $line | awk -F "=" {'print $1'})
+    value=$(echo $line | awk -F "=" {'print $2'})
+    [[ $key == qt6 ]] && QT_VERSION=$value
+    [[ $key == QT_PATH ]] && QT_PATH=$value
+done
+
+if [[ -z $QT_VERSION ]]
+then
+    >&2 echo "Can't read Qt version from $conan_profile"
+    exit 3
+fi
+
+echo Using Qt version $QT_VERSION at $QT_PATH
+
+cd $JOBDIR
+if [[ ! -f conanfile.py ]]
+then
+    >&2 echo "No conanfile.py file found in $JOBDIR"
+    exit 4
+fi
+
+module_name=$(basename $JOBDIR)
+echo "Exporting module $module_name"
+conan export . $module_name/$QT_VERSION@qt/testing
+
+cd $HOME
+[[ -d conan_test ]] && rm -rf conan_test
+mkdir conan_test
+cd conan_test
+
+echo "Installing module $module_name with profile $conan_profile"
+conan install $module_name/$QT_VERSION@qt/testing --build=missing --profile=$conan_profile
