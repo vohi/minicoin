ab95575ddf8c3d82f0a037206166f35b24e289d5
Volker Hilsheimer <volker.hilsheimer@qt.io>
2021-02-25 11:57:20 +0100

    Adjust the test to the new job runner
    Always mount minicoin, otherwise we can't parse arguments.

--- minicoin/tests/runner.sh
+++ minicoin/tests/runner.sh
@@ -157,40 +157,49 @@ fi
 echo "============================> Testing job running <============================"
 machines=( "${@}" )
 
+MINICOIN_EXPERIMENTAL=run
+
 count=${#machines[@]}
 echo "Running test on $count machines independently"
 
 for machine in "${machines[@]}"
 do
     echo "---- $machine - stdout"
-    stdout="$(minicoin run --no-color $machine test 2> /dev/null | grep 'Hello')"
+    stdout="$(echo 0 | minicoin run --no-color test $machine 2> /dev/null | grep 'Hello')"
     return=$?
     assert $return 0
     assert "$stdout" "Hello runner!"
 
     echo "---- $machine - stderr"
-    stderr="$(minicoin run --no-color $machine test -- error 2>&1 > /dev/null | grep 'error code')"
+    stderr="$(echo 0 | minicoin run --no-color test $machine -- error 2>&1 > /dev/null | grep 'error code')"
     return=$?
     assert $return 1
-    assert "$stderr" "Exiting with error code 1"
-    [ $errors -gt 0 ] && printf "${RED}Finished on $machine with $errors errors!${NOCOL}\n"
+    expected="Exiting with error code 1"
+    [[ $count -gt 1 ]] && expected="==> $machine: $expected"
+    assert "$stderr" "$expected"
+    [[ $errors -gt 0 ]] && printf "${RED}Finished on $machine with $errors errors!${NOCOL}\n"
 done
 
 if [ $count -gt 1 ]
 then
-    echo "Running test on $count machines sequentially"
-    minicoin run ${machines[@]} test -- error 2> /dev/null > /dev/null
-    return=$?
-    assert $return $count
+    if [[ $errors -gt 0 ]]
+    then
+        printf "${RED}Skipping advanced tests due to earlier errors${NOCOL}\n"
+    else
+        echo "Running test on $count machines sequentially"
+        minicoin run --jobconfig 0 test ${machines[@]} -- error 2> /dev/null > /dev/null
+        return=$?
+        assert $return $count
 
-    echo "Running test on $count machines in parallel"
-    minicoin run --parallel "${machines[@]}" test 2> /dev/null > /dev/null
-    return=$?
-    assert $return 0
+        echo "Running test on $count machines in parallel"
+        minicoin run --parallel --jobconfig 0 test "${machines[@]}" 2> /dev/null > /dev/null
+        return=$?
+        assert $return 0
 
-    minicoin run --parallel "${machines[@]}" test -- error 2> /dev/null > /dev/null
-    return=$?
-    assert $return $count
+        minicoin run --parallel --jobconfig 0 test "${machines[@]}" -- error 2> /dev/null > /dev/null
+        return=$?
+        assert $return $count
+    fi
 fi
 
 finish
