00092bda09933222fb5078fee5ed11442ff48a53
Volker Hilsheimer <volker.hilsheimer@qt.io>
2021-03-14 16:01:37 +0100

    Turn off notifications
    Do it as a regular provisioning step for now, add it to the basebox
    script.

--- minicoin/roles/windows-setup/provision.ps1
+++ minicoin/roles/windows-setup/provision.ps1
@@ -1,14 +1,3 @@
-net stop wuauserv
-
-$tempfolders = @( "C:\Windows\Temp\*",
-                  "C:\Windows\Prefetch\*",
-                  "C:\Users\*\Appdata\Local\Temp\*",
-                  "C:\Windows\SoftwareDistribution\Downloads\*" )
-
-Remove-Item $tempfolders -force -recurse
-
-Optimize-Volume -DriveLetter C -Defrag
-
 function Set-RegistryKey
 {
     param(
