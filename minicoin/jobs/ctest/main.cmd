5608b6a6cfdcd8242aac20f512b82a28936a525c
Volker Hilsheimer <volker.hilsheimer@qt.io>
2021-10-22 13:00:21 +0200

    Implement a job for running ctest on the guest
    With ctest we can run groups of tests very easily, and in theory we
    could just use the cmd command for doing that. However, we need the run
    command's support for running scripts under an interactive account so
    tests that require UI can pass.

---
+++ minicoin/jobs/ctest/main.cmd
@@ -0,0 +1,51 @@
+@echo off
+SETLOCAL ENABLEDELAYEDEXPANSION
+
+call \minicoin\util\parse-opts.cmd %*
+call \minicoin\util\discover-make.cmd
+
+if NOT DEFINED JOBDIR (
+    >&2 echo Error: path to host clone of Qt is required!
+    exit /B 1
+)
+
+if NOT "!PARAM_build!"=="" (
+    echo "Starting from !PARAM_build!"
+    cd %USERPROFILE%/!PARAM_build!
+) else (
+    cd C:\
+    set find_build=1
+)
+
+:down
+for /F "tokens=1 delims=\" %%a in ("!JOBDIR!") do (
+    set "segment=%%a"
+)
+if NOT "%segment%"=="C:" (
+    if DEFINED find_build (
+        if "!PARAM_build!"=="" (
+            set "buildsegment=%segment%-build"
+        ) else (
+            set "buildsegment=!PARAM_build!"
+        )
+        if exist "%USERPROFILE%\!buildsegment!" (
+            cd "%USERPROFILE%\!buildsegment!"
+            set find_build=
+        )
+    )
+    if NOT DEFINED find_build (
+        if exist %segment% cd %segment%
+    )
+)
+set "OLDJOBDIR=!JOBDIR!"
+set JOBDIR=!JOBDIR:*%segment%\=!
+if "!OLDJOBDIR!"=="!JOBDIR!" goto :there
+if NOT "!JOBDIR!"=="" goto :down
+
+:there
+
+SET error=0
+set TESTARGS=!PARAM_testargs!
+
+echo Running ctest !PASSTHROUGH[@]! in %cd%
+ctest "!PASSTHROUGH[@]!"
---
+++ minicoin/jobs/ctest/main.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+. /minicoin/util/parse-opts.sh "$@"
+
+if [ -z $JOBDIR ]
+then
+  >&2 echo "Error: path to host clone of Qt is required!"
+  exit 1
+fi
+
+if [ ! -z $PARAM_build ]
+then
+    cd $PARAM_build
+else
+    cd /
+    find_build=1
+fi
+
+IFS='/' read -ra SEGMENTS <<< "$JOBDIR"
+for segment in "${SEGMENTS[@]}"; do
+    [ -z $segment ] && continue
+
+    if [ ! -z $find_build ]
+    then
+        if [ -z $PARAM_build ]
+        then
+            buildsegment=$segment-build
+        else
+            buildsegment=$PARAM_build
+        fi
+
+        if [ -d "$HOME/$buildsegment" ]
+        then
+            find_build=
+            cd "$HOME/$buildsegment"
+            continue
+        fi
+    fi
+
+    cd $segment 2> /dev/null
+done
+
+echo "Running ctest ${PASSTHROUGH[@]} in $PWD"
+ctest ${PASSTHROUGH[@]}
