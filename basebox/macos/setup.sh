2241d6f56d40cdb0a9ba13aaefc5e054337594d7
Volker Hilsheimer <volker.hilsheimer@qt.io>
2021-11-19 11:28:20 +0100

    Turn on keyboard navigtation setting on macOS

--- basebox/macos/setup.sh
+++ basebox/macos/setup.sh
@@ -3,6 +3,9 @@ curl -o ~/.ssh/authorized_keys https://raw.githubusercontent.com/hashicorp/vagra
 chmod 0700 ~/.ssh
 chmod 0644 ~/.ssh/authorized_keys
 
+# turn keyboard navigation to all controls on
+defaults write "Apple Global Domain" AppleKeyboardUIMode 2
+
 echo "If this asks for a password, add the following via visudo"
 echo "vagrant ALL=(ALL) NOPASSWD: ALL
 sudo rm -rf /Library/Caches/*
