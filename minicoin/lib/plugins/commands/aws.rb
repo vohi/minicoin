858b7504572ff8e460d10b436882757151da05de
Volker Hilsheimer <volker.hilsheimer@qt.io>
2022-02-05 14:39:31 +0100

    Add command to package instance into AMI
    Optionally, clean up the disk before packaging. Not well tested yet.

--- minicoin/lib/plugins/commands/aws.rb
+++ minicoin/lib/plugins/commands/aws.rb
@@ -1,8 +1,19 @@
+require 'json'
+
 module Minicoin
     module Commands
         class AWS < Vagrant.plugin("2", :command)
             def self.synopsis
                 "Manage AWS instances"
+            end
+
+            def self.echo(ui, type, data)
+                data.chomp! # remove trailing newlines
+                if type == :stderr
+                    ui.error data
+                else
+                    ui.success data
+                end
             end
 
             def initialize(argv, env)
@@ -12,6 +23,9 @@ module Minicoin
                 @subcommands = Vagrant::Registry.new
                 @subcommands.register(:instance) do
                     Instance
+                end
+                @subcommands.register(:package) do
+                    Package
                 end
             end
 
@@ -41,6 +55,7 @@ module Minicoin
                 command_class.new(@sub_args, @env).execute
             end
         end
+
         class Instance < Vagrant.plugin("2", :command)
             def self.synopsis
                 "Print the instance ID of the machine"
@@ -64,16 +79,12 @@ module Minicoin
                 return if !argv
 
                 with_target_vms(argv) do |vm|
-                    ui_options = {
-                        :target => vm.name,
-                        :channel => "foo",
-                        :type => "box-name"
-                    }
-                    ui = @env.ui.is_a?(Vagrant::UI::MachineReadable) ? @env.ui : vm.ui
+                    ui_options = { :target => vm.name }
+                    ui_options = { :prefix => false } if @env.ui.is_a?(Vagrant::UI::MachineReadable)
                     if vm.box.provider != :aws
-                        ui.error "This machine is not backed by the AWS provider (#{vm.box.provider})", **ui_options
+                        vm.ui.error "This machine is not backed by the AWS provider (#{vm.box.provider})", **ui_options
                     elsif !vm.id
-                        ui.error "The machine hasn't been created", **ui_options
+                        vm.ui.error "The machine hasn't been created", **ui_options
                     else
                         if options[:format] == :json
                             data = {
@@ -81,7 +92,77 @@ module Minicoin
                             }
                             STDOUT.puts data.to_json
                         else
-                            ui.machine("box-name", "#{vm.id}", **ui_options)
+                            vm.ui.machine("instance-id", "#{vm.id}", **ui_options)
+                            vm.ui.info vm.id, **ui_options
+                        end
+                    end
+                end
+            end
+        end
+
+        class Package < Vagrant.plugin("2", :command)
+            def self.synopsis
+                "Package the instance into a new AMI"
+            end
+
+            def execute()
+                options = {}
+                parser = OptionParser.new do |option|
+                    option.banner = "Usage: minicoin aws package [options] [name|id]"
+                    option.separator ""
+                    option.separator Instance::synopsis
+                    option.separator ""
+                    option.separator "Options:"
+                    option.separator ""
+                    option.on("--clean", "Clean the running instance before packaging") do
+                        options[:clean] = true
+                    end
+                    option.on("--json", "Enable output as JSON") do
+                        options[:format] = :json
+                    end
+                    option.on("--name NAME", "Specify the name of the AMI") do |o|
+                        options[:name] = o
+                    end
+                end
+
+                argv = parse_options(parser)
+                return if !argv
+
+                with_target_vms(argv) do |vm|
+                    ui_options = { :target => vm.name }
+                    ui_options = { :prefix => false } if @env.ui.is_a?(Vagrant::UI::MachineReadable)
+
+                    if options[:clean]
+                        if vm.state.id != :running
+                            vm.ui.error "Instance is not running and cannot be cleaned before packaging; aborting"
+                            next
+                        end
+                        vm.ui.info "Cleaning up disk space before packaging..."
+                        if vm.guest.name == :windows
+                            vm.communicate.execute("del /Q %TEMP%\\*.*")
+                            vm.communicate.execute("del /Q %SystemRoot%\\Temp\\*.*")
+                            vm.communicate.execute("sdelete64 -z c:") do |type, data|
+                            end
+                            vm.communicate.execute("del /Q %programdata%\\ssh\\administrators_authorized_keys")
+                        else
+                            vm.communicate.execute("rm -rf /tmp/* || true")
+                            vm.communicate.execute("cat /dev/zero > wipeout; rm wipeout")
+                            vm.communicate.execute("rm ~/.ssh/authorized_keys 2> /dev/null || true")
+                        end
+                        vm.ui.detail "... done"
+                    end
+
+                    stdout, stderr, status = vm.provider.call(:ec2, "create-image", {
+                        "instance-id" => vm.id,
+                        :name => options[:name] || "minicoin packaged #{vm.name}"
+                    })
+                    if status != 0
+                        vm.ui.error "Failed to create an AMI from #{vm.name}: #{stderr}", **ui_options
+                    else
+                        if options[:format] == :json
+                            STDOUT.puts stdout
+                        else
+                            vm.ui.success "Package creation initiated: #{JSON.parse(stdout)["ImageId"]}", **ui_options
                         end
                     end
                 end
