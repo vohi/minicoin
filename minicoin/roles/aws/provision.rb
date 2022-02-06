c8ff66e9ab999a42291354c135d8361c4b8c9a40
Volker Hilsheimer <volker.hilsheimer@qt.io>
2022-02-06 17:30:15 +0100

    AWS provisioner: Support region_config

--- minicoin/roles/aws/provision.rb
+++ minicoin/roles/aws/provision.rb
@@ -1,14 +1,29 @@
 def aws_provision(box, name, args, machine)
-    if !args.is_a?(Hash)
-        raise "Argument error: expecting args to be a hash"
-    end
+    raise "Argument error: aws provider configuration needs to be a hash" unless args.is_a?(Hash)
     box.vm.provider :aws do |aws, override|
         args.each do |key, value|
-            next if key == "role" or key == "role_path"
-            if value.is_a?(Array) || value.is_a?(Hash)
-                eval("aws.#{key} = #{value}")
+            case key
+            when "role", "role_path"
+            when "region_config"
+                raise "Argument error: region_config needs to be a hash" unless value.is_a?(Array)
+                value.each do |region_config|
+                    raise "Argument error: each region_config needs to be a hash 'name => {...}'" unless region_config.is_a?(Hash)
+                    region_config.each do |name, settings|
+                        aws.region_config "#{name}" do |region|
+                            if value.is_a?(Array) || value.is_a?(Hash)
+                                eval("region.#{key} = #{value}")
+                            else
+                                eval("region.#{key} = \"#{value}\"")
+                            end            
+                        end
+                    end
+                end
             else
-                eval("aws.#{key} = \"#{value}\"")
+                if value.is_a?(Array) || value.is_a?(Hash)
+                    eval("aws.#{key} = #{value}")
+                else
+                    eval("aws.#{key} = \"#{value}\"")
+                end
             end
         end
     end
