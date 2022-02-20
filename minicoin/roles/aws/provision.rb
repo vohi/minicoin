eba10564c3d468c19a12173b1315f3da6b165056
Volker Hilsheimer <volker.hilsheimer@qt.io>
2022-02-20 16:03:02 +0100

    AWS: Support a simplified list of storage entries
    Just a list of sizes or snapshot IDs for block devices mapped before
    booting the machine, or volume IDs for volumes attached after booting.
    
    Volumes that get attached after booting do not (yet?) get mounted
    automatically, they might not have a file system yet.
    
    The generic "list of sizes" setting might become a generally available
    configuration (but vagrant's disk configuration is still experimental),
    and perhaps the first size should always become the
    system volume's size.

--- minicoin/roles/aws/provision.rb
+++ minicoin/roles/aws/provision.rb
@@ -3,6 +3,49 @@ def aws_provision(box, name, args, machine)
     box.vm.provider :aws do |aws, override|
         args.each do |key, value|
             case key
+            when "storage"
+                # all storage entries become EBS volumes, so start with /dev/sdf
+                storage_device = 'f'
+                value = [value] unless value.is_a?(Array)
+                block_device_mappings = []
+                attached_volumes = []
+                value.each do |storage|
+                    block_device = {}
+                    device_name = "/dev/sd#{storage_device}"
+                    if storage.is_a?(Hash)
+                        if storage["volume"]
+                            attached_volumes << {
+                                "volume-id" => storage['volume'],
+                                "device" => device_name
+                            }
+                        else
+                            block_device["DeviceName"] = storage["device"] || device_name
+                            block_device["Ebs.VolumeSize"] = storage["size"] if storage["size"]
+                            block_device["Ebs.DeleteOnTermination"] = storage["deleteOnTermination"] if storage["deleteOnTermination"]
+                            block_device["Ebs.SnapshotId"] = storage["snapshot"] if storage["snapshot"]
+                        end
+                        storage_device.next! unless storage.has_key?("DeviceName")
+                    else
+                        block_device["Ebs.VolumeSize"] = storage
+                        block_device["DeviceName"] = device_name
+                        storage_device.next!
+                    end
+                    block_device_mappings << block_device unless block_device.empty?
+                end
+                unless attached_volumes.empty?
+                    attach_volumes = lambda do |machine|
+                        attached_volumes.each do |volume|
+                            machine.ui.detail "Attaching #{volume['volume-id']} as #{volume['device']}"
+                            volume["instance-id"] = machine.id
+                            stdout, stderr, status = machine.provider.class.call(:ec2, "attach-volume", volume)
+                            machine.ui.error "Error attaching volume #{volume['volume-id']}: #{stderr}" if status != 0
+                        end
+                    end
+                    box.vm.provision "attach-volume",
+                        type: :local_command,
+                        code: attach_volumes
+                end
+                aws.block_device_mapping = block_device_mappings unless block_device_mappings.empty?
             when "region_config"
                 raise "Argument error: region_config needs to be a hash" unless value.is_a?(Array)
                 value.each do |region_config|
