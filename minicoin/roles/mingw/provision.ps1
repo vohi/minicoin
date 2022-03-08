47fe23e490fe5d7b4576c8d14fc74bcc18cb9f6f
Volker Hilsheimer <volker.hilsheimer@qt.io>
2022-03-08 22:39:12 +0100

    Bump mingw to 11.2.0.07112021
    Add bin directory first to PATH, otherwise this seems to conflict with
    some other toolchain, possibly the one from strawberryperl.

--- minicoin/roles/mingw/provision.ps1
+++ minicoin/roles/mingw/provision.ps1
@@ -1,8 +1,12 @@
 param (
-    [string]$version = "10.2.0",
+    [string]$version = "11.2.0.07112021",
     [string]$package = "mingw"
 )
 
 . "c:\opt\minicoin\util\install_helper.ps1"
 
 Install-Software $package $version
+
+$path=[Environment]::GetEnvironmentVariable("PATH", 'machine')
+[Environment]::SetEnvironmentVariable("PATH", "C:\ProgramData\chocolatey\lib\mingw\tools\install\mingw64\bin;${path}", 'machine')
+refreshenv
